bb2330e83dcfe30a4e3b8256b80cd907b2081d23
JinaStrickland <Jina30@gmail.com>
2020-11-03 12:59:06 -0500

    done

--- src/containers/MainContainer.js
+++ src/containers/MainContainer.js
@@ -5,27 +5,80 @@ import SearchBar from '../components/SearchBar'
 
 class MainContainer extends Component {
 
+  state = {
+    stocks: [],
+    portfolioStocks: [],
+    filterType: "All"
+  }
+
+  componentDidMount() {
+    fetch("http://localhost:3000/stocks")
+    .then(res => res.json())
+    .then(stocks => this.setState({ stocks: stocks }))
+  }
+
+  clickStock = (stock) => {
+    if(!this.state.portfolioStocks.includes(stock)) {
+      this.setState({
+        portfolioStocks: [...this.state.portfolioStocks, stock] 
+      })
+    }
+  }
+
+  clickToRemove = (stock) => {
+    this.setState({
+      portfolioStocks: [...this.state.portfolioStocks].filter(pStock => pStock !== stock)
+    })
+  }
+
+  sortBy = (type) => {
+    if(type === "Alphabetically") {
+      this.setState({
+        stocks: [...this.state.stocks].sort((a, b) => a.name.localeCompare(b.name))
+      })
+    } else if (type === "Price") {
+      this.setState({
+        stocks: [...this.state.stocks].sort((a, b) => a.price - b.price)
+      })
+    }
+  }
+
+  filterBy = (type) => {
+    this.setState({
+      filterType: type 
+    }) 
+  }
+
+  displayStocks = () => {
+    if(this.state.filterType === "All") { 
+      return [...this.state.stocks] 
+    } else {
+      let stocks = [...this.state.stocks].filter(stock => stock.type.includes(this.state.filterType))
+      return stocks 
+    } 
+  }
+  
   render() {
+    
     return (
       <div>
-        <SearchBar/>
-
+        <SearchBar  sortBy={ this.sortBy }
+                    filterBy={ this.filterBy } 
+                    />
           <div className="row">
             <div className="col-8">
-
-              <StockContainer/>
-
+              <StockContainer stocks={ this.displayStocks() }
+                              clickStock={ this.clickStock }/>
             </div>
             <div className="col-4">
-
-              <PortfolioContainer/>
+              <PortfolioContainer portfolioStocks={ this.state.portfolioStocks }
+                                  clickToRemove={ this.clickToRemove } />
 
             </div>
           </div>
       </div>
     );
   }
-
 }
 
 export default MainContainer;
--- src/containers/StockContainer.js
+++ src/containers/StockContainer.js
@@ -7,13 +7,12 @@ class StockContainer extends Component {
     return (
       <div>
         <h2>Stocks</h2>
-        {
-          //render the list of stocks here
-        }
+        { this.props.stocks.map(stock => <Stock key={ stock.id }
+                                                stock={ stock } 
+                                                clickStock={ this.props.clickStock } />)}
       </div>
     );
   }
-
 }
 
 export default StockContainer;
